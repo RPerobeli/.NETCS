ea3304af41914b5441aac1c37a297ba19c556174
RPerobeli <rodrigo.costa2015@engenharia.ufjf.br>
2021-02-17 14:19:57 -0300

    feat: minha primeira aplicacao .NET

---
+++ PrimeiraAplicacao/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PrimeiraAplicacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numVezes = 5;
+            for(int i=0;i<numVezes;i++)
+            {
+                Console.WriteLine($"Bem vindo {i}");
+            }
+            
+        }
+    }
+}
